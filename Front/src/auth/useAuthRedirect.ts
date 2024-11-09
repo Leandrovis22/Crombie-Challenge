c1dd4c17dd9eb73adb795248758a323cf6a0d74c
Leandro <leandroviscolungo@gmail.com>
2024-11-09 04:38:33 -0300

    typed useAuthRedirect.ts

--- Front/src/auth/useAuthRedirect.ts
+++ Front/src/auth/useAuthRedirect.ts
@@ -1,8 +1,13 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { verifyToken } from './authService';
+import { User } from '../types/types';
 
-export const useAuthRedirect = (setData: any) => {
+interface HomeData {
+  user: User;
+}
+
+export const useAuthRedirect = (setData: (data: HomeData | null) => void) => {
   const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
